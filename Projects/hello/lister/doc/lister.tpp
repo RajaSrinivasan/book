269fcaea4c6de02f634dfdb4e5398fd652478a4d
R Srinivasan <rs@toprllc.com>
2023-03-15 06:42:18 -0400

    structure to build book

---
+++ Projects/hello/lister/doc/lister.tpp
@@ -0,0 +1,18 @@
+\section{Lister}
+
+We begin by getting some text input and output experience. The goal of this project is to
+simply printout the contents of a named file - not unlike the familiar \textbf{cat}. However
+the \textbf{lister} will keep track of line numbers and print them as well. The exercise 
+gets us exploring several predefined libraries which are part of the language.
+
+\subsection{Library references}
+
+#include "../src/lister-env.tex"
+
+\subsection{Command Line}
+
+#include "../src/lister-cmdline.tex"
+
+\subsection{The main loop}
+
+#include "../src/lister-mainloop.tex"
--- Projects/hello/lister/src/lister.adb
+++ Projects/hello/lister/src/lister.adb
@@ -1,6 +1,9 @@
+-- codemd: begin segment=env caption=Predefined Language Environment
 with Ada.Command_Line;    use Ada.Command_Line;
 with Ada.Text_IO;         use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+-- codemd: end
+
 
 procedure Lister is
    Inpfile : Ada.Text_IO.File_Type;
@@ -8,13 +11,16 @@ procedure Lister is
    Linelen : Natural;
    Lineno  : Integer := 0;
 begin
+
+-- codemd: begin segment=cmdline caption=Retrieve command line arguments
    if Ada.Command_Line.Argument_Count < 1 then
       Put_Line ("usage: lister <filename>");
       return;
    end if;
+-- codemd: end
 
+-- codemd: begin segment=mainloop caption=Main loop
    Open (Inpfile, In_File, Ada.Command_Line.Argument (1));
-
    while not End_Of_File (Inpfile) loop
       Get_Line (Inpfile, Line, Linelen);
       Lineno := Lineno + 1;
@@ -23,4 +29,5 @@ begin
       Put_Line (Line (1 .. Linelen));
    end loop;
    Ada.Text_IO.Close (Inpfile);
+-- codemd: end
 end Lister;
